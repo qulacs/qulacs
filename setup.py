2ac7cfff4cf7f3793073474fd71b59f3567da160
corryvrequan <corryvrequan@gmail.com>
2018-12-22 13:23:31 +0900

    switch lib output directory with pip or cmake

--- setup.py
+++ setup.py
@@ -33,7 +33,8 @@ class CMakeBuild(build_ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
                       '-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+                      '-DPYTHON_EXECUTABLE=' + sys.executable,
+                      '-DPYTHON_SETUP_FLAG:STR=Yes']
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
