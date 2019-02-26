0c141efc7f0e46b24430ce483a7e4c964cde6107
corryvrequan <corryvrequan@gmail.com>
2019-02-27 00:48:37 +0900

    make compatible with gcc

--- src/cppsim/state.hpp
+++ src/cppsim/state.hpp
@@ -456,7 +456,12 @@ public:
 	 * \~japanese-en 複素数をかける
 	 */
 	virtual void multiply_coef(CPPCTYPE coef) {
+#ifdef _MSC_VER
 		state_multiply(coef, this->data_c(), this->dim);
+#else
+		CTYPE c_coef = {coef.real(), coef.imag()};
+		state_multiply(c_coef, this->data_c(), this->dim);
+#endif
 	}
 
 
