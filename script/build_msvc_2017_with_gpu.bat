84e31ac71cf33413d3070601edb08a3127eecb09
corryvrequan <corryvrequan@gmail.com>
2019-10-14 13:49:14 +0000

    fix install bugs in windows-gpu, fix curand flag for gpu

---
+++ script/build_msvc_2017_with_gpu.bat
@@ -0,0 +1,7 @@
+mkdir visualstudio
+cd visualstudio
+cmake -G "Visual Studio 15 2017 Win64" -D USE_GPU:STR=Yes ..
+cd ..
+cmake --build ./visualstudio --target ALL_BUILD --config Release
+cmake --build ./visualstudio --target python --config Release
+
