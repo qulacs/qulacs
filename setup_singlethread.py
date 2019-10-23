4dbebbc9ef87bfce42fd90e5b89c0f524a08615f
yibe <33653106+yoooopeeee@users.noreply.github.com>
2019-10-23 16:44:12 +0900

    v0.1.8

--- setup_singlethread.py
+++ setup_singlethread.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
 from distutils.version import LooseVersion
 
-_VERSION = '0.1.6'
+_VERSION = '0.1.8'
 
 project_name = 'Qulacs'
 
