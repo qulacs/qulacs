7fb3c35a4da7c8196ba27ab752f19151d5e43457
YouyuanS <49753451+YouyuanS@users.noreply.github.com>
2020-04-06 21:11:19 +0900

    Update gate_named.hpp

--- qulacs-src/cppsim/gate_named.hpp
+++ qulacs-src/cppsim/gate_named.hpp
@@ -9,6 +9,9 @@
 /**
  * \~japanese-en 1量子ビットを対象とする回転角固定のゲートのクラス
  */
+/**
+ * \~english Class of fixed rotation angle gate for 1 qubit
+ */
 class QuantumGate_OneQubit : public QuantumGateBase{
 protected:
     typedef void (T_UPDATE_FUNC)(UINT, CTYPE*, ITYPE);
@@ -24,6 +27,11 @@ public:
      * \~japanese-en 量子状態を更新する
      * 
      * @param state 更新する量子状態
+     */
+    /**
+     * \~english Update quantum state
+     * 
+     * @param state Quantum state to be updated
      */
     virtual void update_quantum_state(QuantumStateBase* state) override{
 		if (state->is_state_vector()) {
@@ -47,6 +55,11 @@ public:
      * 
      * @return 自身のディープコピー
      */
+    /**
+     * \~english Generate a deep copy of itself
+     * 
+     * @return Deep copy of itself
+     */
     virtual QuantumGateBase* copy() const override{
         return new QuantumGate_OneQubit(*this);
     };
@@ -55,6 +68,11 @@ public:
      * 
      * @param matrix 行列をセットする変数の参照
      */
+    /**
+     * \~english Set gate matrix of itself
+     * 
+     * @param matrix Reference variables to set matrix
+     */
     virtual void set_matrix(ComplexMatrix& matrix) const override{
         matrix = this->_matrix_element;
     }
@@ -62,6 +80,9 @@ public:
 
 /**
  * \~japanese-en 2量子ビットを対象とする回転角固定のゲートのクラス
+ */
+/**
+ * \~english Class of fixed rotation angle gate for 2 qubits
  */
 class QuantumGate_TwoQubit : public QuantumGateBase{
 protected:
@@ -78,6 +99,11 @@ public:
      * \~japanese-en 量子状態を更新する
      * 
      * @param state 更新する量子状態
+     */
+    /**
+     * \~english Update quantum state
+     * 
+     * @param state Quantum state to be updated
      */
     virtual void update_quantum_state(QuantumStateBase* state) override{
 		if (state->is_state_vector()) {
@@ -101,6 +127,11 @@ public:
      * 
      * @return 自身のディープコピー
      */
+    /**
+     * \~english Generate a deep copy of itself
+     * 
+     * @return Deep copy of itself
+     */
     virtual QuantumGateBase* copy() const override{
         return new QuantumGate_TwoQubit(*this);
     };
@@ -109,6 +140,11 @@ public:
      * 
      * @param matrix 行列をセットする変数の参照
      */
+    /**
+     * \~english Set gate matrix of itself
+     * 
+     * @param matrix Reference variables to set matrix
+     */
     virtual void set_matrix(ComplexMatrix& matrix) const override {
         matrix = this->_matrix_element;
     }
@@ -116,6 +152,9 @@ public:
 
 /**
  * \~japanese-en 1量子ビットを対象とし1量子ビットにコントロールされる回転角固定のゲートのクラス
+ */
+/**
+ * \~english Class of fixed rotation angle gate controlled by 1 qubit for 1 qubit
  */
 class QuantumGate_OneControlOneTarget : public QuantumGateBase {
 protected:
@@ -132,6 +171,11 @@ public:
      * \~japanese-en 量子状態を更新する
      * 
      * @param state 更新する量子状態
+     */
+    /**
+     * \~english Update quantum state
+     * 
+     * @param state Quantum state to be updated
      */
     virtual void update_quantum_state(QuantumStateBase* state) override {
 		if (state->is_state_vector()) {
@@ -155,6 +199,11 @@ public:
      * 
      * @return 自身のディープコピー
      */
+    /**
+     * \~english Generate a deep copy of itself
+     * 
+     * @return Deep copy of itself
+     */
     virtual QuantumGateBase* copy() const override {
         return new QuantumGate_OneControlOneTarget(*this);
     };
@@ -163,6 +212,11 @@ public:
      * 
      * @param matrix 行列をセットする変数の参照
      */
+    /**
+     * \~english Set gate matrix of itself
+     * 
+     * @param matrix Reference variables to set matrix
+     */
     virtual void set_matrix(ComplexMatrix& matrix) const override {
         matrix = this->_matrix_element;
     }
@@ -170,6 +224,9 @@ public:
 
 /**
  * \~japanese-en 1量子ビットを対象とする回転ゲートのクラス
+ */
+/**
+ * \~english Class of rotation gate for one qubit
  */
 class QuantumGate_OneQubitRotation : public QuantumGateBase{
 protected:
@@ -187,6 +244,11 @@ public:
      * \~japanese-en 量子状態を更新する
      * 
      * @param state 更新する量子状態
+     */
+    /**
+     * \~english Update quantum state
+     * 
+     * @param state Quantum state to be updated
      */
     virtual void update_quantum_state(QuantumStateBase* state) override{
 		if (state->is_state_vector()) {
@@ -210,6 +272,11 @@ public:
      * 
      * @return 自身のディープコピー
      */
+    /**
+     * \~english Generate a deep copy of itself
+     * 
+     * @return Deep copy of itself
+     */
     virtual QuantumGateBase* copy() const override{
         return new QuantumGate_OneQubitRotation(*this);
     };
@@ -217,6 +284,11 @@ public:
      * \~japanese-en 自身のゲート行列をセットする
      * 
      * @param matrix 行列をセットする変数の参照
+     */
+    /**
+     * \~english Set gate matrix of itself
+     * 
+     * @param matrix Reference variables to set matrix
      */
     virtual void set_matrix(ComplexMatrix& matrix) const override {
         matrix = this->_matrix_element;
