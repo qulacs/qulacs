d9f976d4d92d08b92fbd9a01441ce3e1cfa2851e
corryvrequan <corryvrequan@gmail.com>
2020-05-01 04:06:27 +0900

    small patch

--- test/csim/test_update_named.cpp
+++ test/csim/test_update_named.cpp
@@ -195,9 +195,9 @@ TEST(UpdateTest, SingleQubitRotationGateTest) {
 	//const ITYPE dim = 1ULL << n;
 	//const UINT max_repeat = 10;
 
-	const UINT n = 3;
+	const UINT n = 1;
 	const ITYPE dim = 1ULL << n;
-	const UINT max_repeat = 11;
+	const UINT max_repeat = 1;
 
 	Eigen::MatrixXcd Identity(2, 2), X(2, 2), Y(2, 2), Z(2, 2);
 	Identity << 1, 0, 0, 1;
@@ -227,11 +227,11 @@ TEST(UpdateTest, SingleQubitRotationGateTest) {
 			auto name = std::get<2>(tup);
 			func(target, angle, state, dim);
 			test_state = get_expanded_eigen_matrix_with_identity(target, cos(angle / 2)*Identity + 1.i*sin(angle / 2)*mat, n) * test_state;
-			//state_equal(state, test_state, dim, name);
 			std::cout << angle << std::endl;
 			std::cout << target << std::endl;
 			std::cout << state << std::endl;
 			std::cout << test_state << std::endl;
+			state_equal(state, test_state, dim, name);
 		}
 	}
 	release_quantum_state(state);
