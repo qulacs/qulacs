7222c00e29c92baa6665cd544c4134427d90ef7a
KAWAKUBO Toru <kawakubo@qunasys.com>
2020-11-27 19:34:40 +0900

    Update version to 0.2.0

--- setup_singlethread.py
+++ setup_singlethread.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
 from distutils.version import LooseVersion
 
-_VERSION = '0.1.10.1'
+_VERSION = '0.2.0'
 
 project_name = 'Qulacs'
 
