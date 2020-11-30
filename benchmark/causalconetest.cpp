1aca72922694247efae7c27b2139983b9bb6ef3e
jupiro <aposti0225@yahoo.co.jp>
2020-11-30 19:41:45 +0900

    causalcone分解処理

--- benchmark/causalconetest.cpp
+++ benchmark/causalconetest.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <chrono>
 #include <iomanip>
+#include <string>
 #include <cppsim/state.hpp>
 #include <cppsim/circuit.hpp>
 #include <cppsim/observable.hpp>
@@ -13,74 +14,23 @@
 
 using namespace std;
 int main(){
-
-	for(UINT n = 1; n <= 2; n++){
-		for(UINT m = 1; m <= 3; m++){
-			for(UINT depth = 1; depth <= 4; depth++){
-				QuantumState state((n * 2) * (m * 2));
-				QuantumCircuit circuit((n * 2) * (m * 2));
-
-				auto encode = [&](UINT h, UINT w)-> UINT{
-					return h * m * 2 + w;
-				};
-				for(UINT d = 0; d < depth;){
-					for(UINT k = 0; k < 2; k++){
-						for(UINT i = 0; i < n; i++){
-							for(UINT j = 0; j < m; j++){
-								{
-									std::vector<UINT> v(2);
-									v[0] = encode(i * 2 + k, j * 2 + k);
-									v[1] = encode(i * 2 + k, (j * 2 + 1 + k) % (m * 2));
-									circuit.add_random_unitary_gate(v);
-								}
-								{
-									std::vector<UINT> v(2);
-									v[0] = encode(i * 2 + k, j * 2 + k);
-									v[1] = encode((i * 2 + 1 + k) % (n * 2), j * 2 + k);
-									circuit.add_random_unitary_gate(v);
-								}
-
-								{
-									std::vector<UINT> v(2);
-									v[0] = encode((i * 2 + 1 + k) % (n * 2), j * 2 + k);
-									v[1] = encode((i * 2 + 1 + k) % (n * 2), (j * 2 + 1 + k) % (m * 2));
-									circuit.add_random_unitary_gate(v);
-								}
-								{
-									std::vector<UINT> v(2);
-									v[0] = encode(i * 2 + k, (j * 2 + 1 + k) % (m * 2));
-									v[1] = encode((i * 2 + 1 + k) % (n * 2), (j * 2 + 1 + k) % (m * 2));
-									circuit.add_random_unitary_gate(v);
-								}
-							}
-						}
-						d++;
-						if(d >= depth) break;
-					}
-				}
-				Observable observable((n * 2) * (m * 2));
-				mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
-				for(UINT i = 0; i < (n * 2) * (m * 2); i++){
-					string s; s+="Z "; s+= to_string(i);
-					observable.add_operator(rnd(), s);
-				}
-	
-				auto start = std::chrono::system_clock::now();
-				circuit.update_quantum_state(&state);
-				auto value = observable.get_expectation_value(&state);
-				auto end = std::chrono::system_clock::now();
-
-				double t1 = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
-				start = std::chrono::system_clock::now();
-				auto v2= CausalCone(circuit, observable);
-				end = std::chrono::system_clock::now();
-				double t2 = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
-				//cout<<"value"<<" "<<value<<" "<<v2<<"\n";
-				cout<< n * 2<<" "<<m * 2 <<" "<<depth * 4 <<" "<<t1<<" "<<t2<<"\n";
-			}
+	const UINT n = 5;
+	const UINT depth = 3;
+	QuantumState state(n * 2);
+	QuantumCircuit circuit(n * 2);
+	for(UINT i = 0; i < depth; i++){
+		for(UINT j = 0; j < n; j++){
+			std::vector<UINT> v = {j * 2, j * 2 + 1};
+			circuit.add_random_unitary_gate(v);
 		}
 	}
+	Observable observable(n * 2);
+	mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+	string s = "Z " + to_string(0) + "Z " + to_string(n);
+	observable.add_operator(rnd(), s);
+	circuit.update_quantum_state(&state);
+	auto value = observable.get_expectation_value(&state);
+	auto value2 = CausalCone(circuit, observable);
+	cout<<value<<" "<<value2<<endl;
 
-	
-	//cout<<v2<<" "<<t2<<endl;
 }
--- src/cppsim/causal_cone.hpp
+++ src/cppsim/causal_cone.hpp
@@ -59,10 +59,12 @@ CPPCTYPE CausalCone(const QuantumCircuit &init_circuit,
 		std::vector<UINT> observable_index_list = term->get_index_list();
 	    const UINT gate_count = init_circuit.gate_list.size();
 	    const UINT qubit_count = init_circuit.qubit_count;
-	    UnionFind uf(qubit_count);
+	    const UINT observable_count = observable_index_list.size();
+	    UnionFind uf(qubit_count + observable_count);
 	    std::vector<bool> use_qubit(qubit_count);
 	    std::vector<bool> use_gate(gate_count);
-	    for(auto observable_index : observable_index_list) {
+	    for(UINT i = 0; i < observable_count; i++) {
+	    	UINT observable_index = observable_index_list[i];
 	        use_qubit[observable_index] = true;
 	    }
 	    for(int i = gate_count - 1; i >= 0; i--){
@@ -104,7 +106,7 @@ CPPCTYPE CausalCone(const QuantumCircuit &init_circuit,
 	    }
 
 	    //termの中では分解しない(仮)
-	    		
+	    /*
 	    {
 	    	std::vector<UINT> qubit_encode(qubit_count, -1);
 	    	std::vector<UINT> qubit_decode(qubit_count, -1);
@@ -148,40 +150,84 @@ CPPCTYPE CausalCone(const QuantumCircuit &init_circuit,
 		    circuit->update_quantum_state(&state);
 		    ret += paulioperator.get_expectation_value(&state);
 	    }
+	    */
+	    //分解処理
 	    
-
-	    //分解処理(オブザーバルの処理どうしよ)
-	    /*
+	    auto term_index_list = term->get_index_list();
+	    auto pauli_id_list = term->get_pauli_id_list();
 	    UINT circuit_count = 0;
-	    std::vector<UINT> enc(qubit_count, -1);
+	    std::vector<UINT> roots;
 	    for(UINT i = 0; i < qubit_count; i++){
-	        if(use_qubit[i] and enc[uf.root(i)] == -1){
-	            enc[uf.root(i)] = circuit_count;
+	        if(use_qubit[i] and i == uf.root(i)){
+	            roots.emplace_back(uf.root(i));
 	            circuit_count += 1;
 	        }
 	    }
+	    std::cout<<"circuit_count:"<<" "<<circuit_count<<"\n";
 	    std::vector<QuantumCircuit*> circuits(circuit_count, nullptr);
+	    CPPCTYPE expectation(1.0, 0);
 	    for(UINT i = 0; i < circuit_count; i++) {
-	    	circuits[i] = new QuantumCircuit(qubit_count);
+	    	UINT root = roots[i];
+	    	circuits[i] = new QuantumCircuit(uf.size(root));
+	    	auto &circuit = circuits[i];
+	    	std::vector<int> qubit_encode(qubit_count, -1);
+	    	
+    		int idx = 0;
+    		for(UINT i = 0; i < qubit_count; i++){
+    			if(root == uf.root(i)){
+    				qubit_encode[i] = idx++;
+    			}
+    		}
+	    	
+	    	for(UINT i = 0; i < gate_count; i++){
+	        	if(!use_gate[i]) continue;
+
+		        auto gate = init_circuit.gate_list[i]->copy();
+		        auto target_index_list = gate->get_target_index_list();
+	        	auto control_index_list = gate->get_control_index_list();
+	        	if(uf.root(target_index_list[0]) != root) continue;
+	        	for(auto &idx : target_index_list) idx = qubit_encode[idx];
+	        	for(auto &idx : control_index_list) idx = qubit_encode[idx];
+
+	        	gate->set_target_index_list(target_index_list);
+	        	gate->set_control_index_list(control_index_list);
+		        circuit->add_gate(gate);
+		    }
+		    PauliOperator paulioperator(1.0);
+		    for(UINT i = 0; i < term_index_list.size(); i++){
+		    	paulioperator.add_single_Pauli(qubit_encode[term_index_list[i]], pauli_id_list[i]);
+			}
+			QuantumState state(idx);
+	    	state.set_zero_state();
+	    	circuit->update_quantum_state(&state);
+		    expectation *= paulioperator.get_expectation_value(&state);
 	    }
-	    for(UINT i = 0; i < gate_count; i++){
-	        if(!use_gate[i]) continue;
-	        auto &gate = init_circuit.gate_list[i];
-	        UINT circuit_index = enc[uf.root(gate->get_target_index_list()[0])];
-	        circuits[circuit_index]->add_gate_copy(gate);
-	    }
-	    CPPCTYPE expectation = CPPCTYPE(1.0, 0.0);
-	    {
-	    	for(auto &circuit : circuits){
-	    		circuit->update_quantum_state(state);
-	    		for(auto &single : term){
-	    			expectation *= 
-	    		}
-	    		expectation *= term->get_expectation_value(state);
-	    	}
-		}
-		ret += expectation;
-		*/
+
+		ret += expectation * term->get_coef();
+		
 	}
+
     return ret;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
