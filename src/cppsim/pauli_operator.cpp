203498135e60abaab99b472ee0faa8644b9e8446
kotamanegi <kotamanegi84@gmail.com>
2020-12-21 20:14:51 +0900

    Fixed PauliOperator constructor to have more error type.

--- src/cppsim/pauli_operator.cpp
+++ src/cppsim/pauli_operator.cpp
@@ -35,8 +35,16 @@ PauliOperator::PauliOperator(std::string strings, CPPCTYPE coef){
     std::string pauli_str;
     UINT index, pauli_type=0;
     while(!ss.eof()){
+        pauli_str.clear();
+        index = UINT_MAX - 1;
         ss >> pauli_str >> index;
-        if (pauli_str.length() == 0) break;
+        if (index == UINT_MAX - 1) {
+            if (pauli_str.empty() == false) {
+                std::cerr << "Warning: PauliOperator::PauliOperator(std::string, CPPCTYPE) : detected pauli_str without indexs. Maybe mistyped?" << std::endl;
+                std::cerr << "Original Pauli string: " << strings << std::endl;
+            }
+            break;
+        }
         if(pauli_str=="I" || pauli_str=="i") pauli_type = 0;
         else if(pauli_str=="X" || pauli_str=="x") pauli_type = 1;
         else if(pauli_str=="Y" || pauli_str=="y") pauli_type = 2;
