03815cfd5b7e55662e20e546a3197ec010583517
kotamanegi <kotamanegi84@gmail.com>
2020-12-24 11:59:25 +0900

    Added parallel compile option to scripts

--- script/build_gcc.sh
+++ script/build_gcc.sh
@@ -29,7 +29,7 @@ fi
 mkdir ./build
 cd ./build
 cmake -G "Unix Makefiles" -D CMAKE_C_COMPILER=$GCC_COMMAND -D CMAKE_CXX_COMPILER=$GXX_COMMAND -D CMAKE_BUILD_TYPE=Release -D USE_GPU:STR=No -D USE_MPI:STR=No ..
-make
+make -j
 make python
 cd ../
 
