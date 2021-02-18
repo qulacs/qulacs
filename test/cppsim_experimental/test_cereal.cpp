f4f17084bae19522fdba78d7c3241967baab480d
kotamanegi <kotamanegi84@gmail.com>
2021-02-18 12:39:28 +0900

    Check Test

--- test/cppsim_experimental/test_cereal.cpp
+++ test/cppsim_experimental/test_cereal.cpp
@@ -12,83 +12,83 @@
 
 TEST(CerealTest, Serialize_ComplexMatrix) {
     // Just Check whether they run without Runtime Errors.
-    StateVector a(6),b(6);
+    StateVector a(6), b(6);
     a.set_zero_state();
     b.set_zero_state();
     {
         ComplexMatrix mat(8, 8);
 
-            mat <<
-1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0;
-        QuantumGateBasic* hoge = QuantumGateBasic::DenseMatrixGate({0,1,2}, mat, {});
-        std::ofstream os("out.cereal", std::ios::binary);
+        mat << 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+            0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+            0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0;
+        QuantumGateBasic* hoge =
+            QuantumGateBasic::DenseMatrixGate({0, 1, 2}, mat, {});
+        std::ofstream os("out1.cereal", std::ios::binary);
         cereal::BinaryOutputArchive archive(os);
         archive(*hoge);
-        hoge -> update_quantum_state(&a);
+        hoge->update_quantum_state(&a);
     }
     {
         QuantumGateBasic* hoge = gate::X(2);
-        std::ifstream is("out.cereal", std::ios::binary);
+        std::ifstream is("out1.cereal", std::ios::binary);
         cereal::BinaryInputArchive archive(is);
         archive(*hoge);
-        hoge -> update_quantum_state(&b);
+        hoge->update_quantum_state(&b);
     }
-    //StateVector applied by QuantumGate should be same. 
-    for(int i = 0;i < (1 << 6);++i){
-        ASSERT_NEAR(abs(a.data_cpp()[i] - b.data_cpp()[i]),0,1e-7);
+    // StateVector applied by QuantumGate should be same.
+    for (int i = 0; i < (1 << 6); ++i) {
+        ASSERT_NEAR(abs(a.data_cpp()[i] - b.data_cpp()[i]), 0, 1e-7);
     }
 }
 
 TEST(CerealTest, Serialize_QuantumGateWrapped) {
     // Just Check whether they run without Runtime Errors.
-    StateVector a(6),b(6);
+    StateVector a(6), b(6);
     a.set_zero_state();
     b.set_zero_state();
     {
-        QuantumGateBase* tmp = gate::TwoQubitDepolarizingNoise(0,1,0.5);
+        QuantumGateBase* tmp = gate::TwoQubitDepolarizingNoise(0, 1, 0.5);
         std::unique_ptr<QuantumGateBase> hoge(tmp);
-        std::ofstream os("out.cereal", std::ios::binary);
+        std::ofstream os("out2.cereal", std::ios::binary);
         cereal::BinaryOutputArchive archive(os);
         archive(hoge);
-        hoge -> update_quantum_state(&a);
+        hoge->update_quantum_state(&a);
     }
     {
         QuantumGateBase* tmp = gate::X(0);
         std::unique_ptr<QuantumGateBase> hoge(tmp);
-        std::ifstream is("out.cereal", std::ios::binary);
+        std::ifstream is("out2.cereal", std::ios::binary);
         cereal::BinaryInputArchive archive(is);
         archive(hoge);
-        hoge -> update_quantum_state(&b);
-        
+        hoge->update_quantum_state(&b);
     }
-    
 }
 
 TEST(CerealTest, serealize_QuantumCircuit) {
     // Just Check whether they run without Runtime Errors.
-    StateVectorCpu a(10),b(10);
+    StateVectorCpu a(10), b(10);
     a.set_zero_state();
     b.set_zero_state();
-    QuantumCircuit inputs(10),outputs(1);
+    QuantumCircuit inputs(10), outputs(1);
     {
-        //serialize QuantumCircuit
+        // serialize QuantumCircuit
         inputs.add_gate(gate::X(0));
         inputs.add_gate(gate::H(1));
         inputs.update_quantum_state(&a);
-        std::ofstream os("out.cereal", std::ios::binary);
+        std::ofstream os("out3.cereal", std::ios::binary);
         cereal::BinaryOutputArchive archive(os);
         archive(inputs);
     }
     {
-        //deserialize QuantumCircuit
-        std::ifstream is("out.cereal", std::ios::binary);
+        // deserialize QuantumCircuit
+        std::ifstream is("out3.cereal", std::ios::binary);
         cereal::BinaryInputArchive archive(is);
         archive(outputs);
         outputs.update_quantum_state(&b);
     }
-    
-    //StateVector applied by QuantumCircuit should be same. 
-    for(int i = 0;i < (1 << 10);++i){
-        ASSERT_NEAR(abs(a.data_cpp()[i] - b.data_cpp()[i]),0,1e-7);
+
+    // StateVector applied by QuantumCircuit should be same.
+    for (int i = 0; i < (1 << 10); ++i) {
+        ASSERT_NEAR(abs(a.data_cpp()[i] - b.data_cpp()[i]), 0, 1e-7);
     }
 }
