d4afcef4e4769ebcb94037d76a337c29299e04fb
Hiroya-W <hyuyukun@gmail.com>
2021-06-15 17:05:48 +0900

    Implement RemoveTermTest

--- test/cppsim_experimental/test_fermion_operator.cpp
+++ test/cppsim_experimental/test_fermion_operator.cpp
@@ -48,3 +48,23 @@ TEST(FermionOperatorTest, AddGetTermTest) {
     EXPECT_EQ(term3.second.get_action_id_list().at(2), term2.second.get_action_id_list().at(2));
 }
 
+TEST(FermionOperatorTest, RemoveTermTest) {
+    FermionOperator fermion_operator;
+    fermion_operator.add_term(1.0, "2^ 1");
+    fermion_operator.add_term(-2.0, "5^ 4 3^");
+    fermion_operator.add_term(3.0, "6 7^");
+
+    EXPECT_EQ(3, fermion_operator.get_term_count());
+    
+    // Removeした結果、Termは1個減る
+    fermion_operator.remove_term(1);
+    EXPECT_EQ(2, fermion_operator.get_term_count());
+
+    auto term = fermion_operator.get_term(1);
+    EXPECT_EQ(3.0, term.first);
+    EXPECT_EQ(6, term.second.get_target_index_list().at(0));
+    EXPECT_EQ(ACTION_DESTROY_ID, term.second.get_action_id_list().at(0));
+    EXPECT_EQ(7, term.second.get_target_index_list().at(1));
+    EXPECT_EQ(ACTION_CREATE_ID, term.second.get_action_id_list().at(1));
+}
+
