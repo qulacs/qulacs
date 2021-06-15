3d2ce14127fdc089338388c565d25f436a5c79dc
Hiroya-W <hyuyukun@gmail.com>
2021-06-15 17:03:46 +0900

    Add tests for other overloaded add_term method

--- test/cppsim_experimental/test_fermion_operator.cpp
+++ test/cppsim_experimental/test_fermion_operator.cpp
@@ -3,7 +3,7 @@
 #include <cppsim_experimental/fermion_operator.hpp>
 #include <cppsim_experimental/observable.hpp>
 
-TEST(FermionOperatorTest, GetTermCountTest){
+TEST(FermionOperatorTest, GetTermCountTest) {
     FermionOperator fermion_operator;
     fermion_operator.add_term(1.0, "2^ 1");
     EXPECT_EQ(1, fermion_operator.get_term_count());
@@ -12,11 +12,14 @@ TEST(FermionOperatorTest, GetTermCountTest){
     EXPECT_EQ(2, fermion_operator.get_term_count());
 }
 
-TEST(FermionOperatorTest, AddGetTermTest){
+TEST(FermionOperatorTest, AddGetTermTest) {
     FermionOperator fermion_operator;
     fermion_operator.add_term(1.0, "2^ 1");
     fermion_operator.add_term(-2.0, "5^ 4 3^");
+    SingleFermionOperator op("5^ 4 3^");
+    fermion_operator.add_term(-2.0, op);
 
+    // AddしたものとGetしたものが一致することを確認する
     auto term1 = fermion_operator.get_term(0);
     EXPECT_EQ(1.0, term1.first);
     EXPECT_EQ(2, term1.second.get_target_index_list().at(0));
@@ -32,5 +35,16 @@ TEST(FermionOperatorTest, AddGetTermTest){
     EXPECT_EQ(ACTION_DESTROY_ID, term2.second.get_action_id_list().at(1));
     EXPECT_EQ(3, term2.second.get_target_index_list().at(2));
     EXPECT_EQ(ACTION_CREATE_ID, term2.second.get_action_id_list().at(2));
+
+    // SingleFermionOperatorを用いてAddした場合と、文字列を用いてAddした場合で同じ結果
+    // が得られることを確認する
+    auto term3 = fermion_operator.get_term(2);
+    EXPECT_EQ(term3.first, term2.first);
+    EXPECT_EQ(term3.second.get_target_index_list().at(0), term2.second.get_target_index_list().at(0));
+    EXPECT_EQ(term3.second.get_action_id_list().at(0), term2.second.get_action_id_list().at(0));
+    EXPECT_EQ(term3.second.get_target_index_list().at(1), term2.second.get_target_index_list().at(1));
+    EXPECT_EQ(term3.second.get_action_id_list().at(1), term2.second.get_action_id_list().at(1));
+    EXPECT_EQ(term3.second.get_target_index_list().at(2), term2.second.get_target_index_list().at(2));
+    EXPECT_EQ(term3.second.get_action_id_list().at(2), term2.second.get_action_id_list().at(2));
 }
 
