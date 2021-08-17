9f80172f259d6a96804cb8080c525d3a7361abd5
Hiroya-W <hyuyukun@gmail.com>
2021-08-17 18:08:13 +0900

    Add Test for bravyi_kitaev

--- test/cppsim_experimental/test_fermion_operator.cpp
+++ test/cppsim_experimental/test_fermion_operator.cpp
@@ -229,3 +229,81 @@ TEST(FermionOperatorTest, parity_setTest){
     std::set<UINT> parity_set_30_expected = {15, 23, 27, 29, 30};
     EXPECT_TRUE(set_compare(parity_set_30, parity_set_30_expected));
 }
+
+TEST(FermionOperatorTest, bravyi_kitaevTest1){
+    FermionOperator op1;
+    op1.add_term(1.0, "17");
+    op1.add_term(1.0, "17^");
+
+    Observable res = op1.bravyi_kitaev(100);
+
+    MultiQubitPauliOperator ex1("Z 15 Y 17 X 19 X 23 X 31 X 63");
+    MultiQubitPauliOperator ex2("Z 15 Z 16 X 17 X 19 X 23 X 31 X 63");
+
+    std::string res_expected =
+    "(0+0j) [Z 15 Y 17 X 19 X 23 X 31 X 63 ] +\n"
+    "(1+0j) [Z 15 Z 16 X 17 X 19 X 23 X 31 X 63 ]";
+
+    EXPECT_EQ(res.get_term_count(), 2);
+    for(int i = 0; i < res.get_term_count(); i++) {
+        auto term = res.get_term(i);
+        if(term.second == ex1)
+            EXPECT_EQ(term.first, 0i);
+        else if(term.second == ex2)
+            EXPECT_EQ(term.first, 1.0);
+        else
+            ADD_FAILURE() << "Result IS EXPECTED:\n" << res_expected << "\nres:\n" << res.to_string();
+    }
+}
+
+TEST(FermionOperatorTest, bravyi_kitaevTest2){
+    FermionOperator op1;
+    op1.add_term(1.0, "50");
+    op1.add_term(1.0, "50^");
+
+    Observable res = op1.bravyi_kitaev(100);
+
+    MultiQubitPauliOperator ex1("Z 31 Z 47 Z 49 Y 50 X 51 X 55 X 63");
+    MultiQubitPauliOperator ex2("Z 31 Z 47 Z 49 X 50 X 51 X 55 X 63");
+
+    std::string res_expected =
+        "(0+0j) [Z 31 Z 47 Z 49 Y 50 X 51 X 55 X 63 ] +\n"
+        "(1+0j) [Z 31 Z 47 Z 49 X 50 X 51 X 55 X 63 ]";
+
+    EXPECT_EQ(res.get_term_count(), 2);
+    for(int i = 0; i < res.get_term_count(); i++) {
+        auto term = res.get_term(i);
+        if(term.second == ex1)
+            EXPECT_EQ(term.first, 0i);
+        else if(term.second == ex2)
+            EXPECT_EQ(term.first, 1.0);
+        else
+            ADD_FAILURE() << "Result IS EXPECTED:\n" << res_expected << "\nres:\n" << res.to_string();
+    }
+}
+
+TEST(FermionOperatorTest, bravyi_kitaevTest3){
+    FermionOperator op1;
+    op1.add_term(1.0, "73");
+    op1.add_term(1.0, "73^");
+
+    Observable res = op1.bravyi_kitaev(100);
+
+    MultiQubitPauliOperator ex1("Z 63 Z 71 Y 73 X 75 X 79 X 95");
+    MultiQubitPauliOperator ex2("Z 63 Z 71 Z 72 X 73 X 75 X 79 X 95");
+
+    std::string res_expected =
+        "(0+0j) [Z 63 Z 71 Y 73 X 75 X 79 X 95 ] +\n"
+        "(1+0j) [Z 63 Z 71 Z 72 X 73 X 75 X 79 X 95 ]";
+
+    EXPECT_EQ(res.get_term_count(), 2);
+    for(int i = 0; i < res.get_term_count(); i++) {
+        auto term = res.get_term(i);
+        if(term.second == ex1)
+            EXPECT_EQ(term.first, 0i);
+        else if(term.second == ex2)
+            EXPECT_EQ(term.first, 1.0);
+        else
+            ADD_FAILURE() << "Result IS EXPECTED:\n" << res_expected << "\nres:\n" << res.to_string();
+    }
+}
