3ea7a4facc1331386b05c4e51e6d2b122693dfb3
Hiroya-W <hyuyukun@gmail.com>
2021-08-20 17:07:51 +0900

    Fixed the test to support order swapping

--- test/cppsim_experimental/test_fermion_operator.cpp
+++ test/cppsim_experimental/test_fermion_operator.cpp
@@ -117,18 +117,63 @@ TEST(FermionOperatorTest, SingleActionJordanWignerTest) {
     Observable term_2_jw = term_2.jordan_wigner();
     Observable term_2hat_jw = term_2hat.jordan_wigner();
 
-    std::string term_0_jw_expected = "(0.5+0j) [X 0 ] +\n(0+0.5j) [Y 0 ]";
-    std::string term_0hat_jw_expected = "(0.5+0j) [X 0 ] +\n(0-0.5j) [Y 0 ]";
-    std::string term_2_jw_expected =
+    // TEST
+
+    MultiQubitPauliOperator x_op("X 0");
+    MultiQubitPauliOperator y_op("Y 0");
+    std::string term_0_jw_exp = "(0.5+0j) [X 0 ] +\n(0+0.5j) [Y 0 ]";
+    EXPECT_EQ(term_0_jw.get_term_count(), 2);
+    for(int i = 0; i < term_0_jw.get_term_count(); i++) {
+        auto term = term_0_jw.get_term(i);
+        if(term.second == x_op)
+            EXPECT_EQ(term.first, 0.5);
+        else if(term.second == y_op)
+            EXPECT_EQ(term.first, 0.5i);
+        else
+            ADD_FAILURE() << "Result IS EXPECTED:" << term_0_jw_exp <<  "\nterm_0_jw:\n" << term_0_jw.to_string();
+    }
+
+    std::string term_0hat_jw_exp = "(0.5+0j) [X 0 ] +\n(0-0.5j) [Y 0 ]";
+    EXPECT_EQ(term_0hat_jw.get_term_count(), 2);
+    for(int i = 0; i < term_0hat_jw.get_term_count(); i++) {
+        auto term = term_0hat_jw.get_term(i);
+        if(term.second == x_op)
+            EXPECT_EQ(term.first, 0.5);
+        else if(term.second == y_op)
+            EXPECT_EQ(term.first, -0.5i);
+        else
+            ADD_FAILURE() << "Result IS EXPECTED:" << term_0hat_jw_exp << "\nterm_0hat_jw:\n" << term_0hat_jw.to_string();
+    }
+
+    std::string term_2_jw_exp =
         "(0.5+0j) [Z 0 Z 1 X 2 ] +\n"
         "(0+0.5j) [Z 0 Z 1 Y 2 ]";
-    std::string term_2hat_jw_expected =
+    MultiQubitPauliOperator op1("Z 0 Z 1 X 2");
+    MultiQubitPauliOperator op2("Z 0 Z 1 Y 2");
+    EXPECT_EQ(term_2_jw.get_term_count(), 2);
+    for(int i = 0; i < term_2_jw.get_term_count(); i++) {
+        auto term = term_2_jw.get_term(i);
+        if(term.second == op1)
+            EXPECT_EQ(term.first, 0.5);
+        else if(term.second == op2)
+            EXPECT_EQ(term.first, 0.5i);
+        else
+            ADD_FAILURE() << "Result IS EXPECTED:" << term_2_jw_exp << "\nterm_2_jw:\n" << term_2_jw.to_string();
+    }
+
+    std::string term_2hat_jw_exp = 
         "(0.5+0j) [Z 0 Z 1 X 2 ] +\n"
         "(0-0.5j) [Z 0 Z 1 Y 2 ]";
-    EXPECT_EQ(term_0_jw.to_string(), term_0_jw_expected);
-    EXPECT_EQ(term_0hat_jw.to_string(), term_0hat_jw_expected);
-    EXPECT_EQ(term_2_jw.to_string(), term_2_jw_expected);
-    EXPECT_EQ(term_2hat_jw.to_string(), term_2hat_jw_expected);
+    EXPECT_EQ(term_2hat_jw.get_term_count(), 2);
+    for(int i = 0; i < term_2hat_jw.get_term_count(); i++) {
+        auto term = term_2hat_jw.get_term(i);
+        if(term.second == op1)
+            EXPECT_EQ(term.first, 0.5);
+        else if(term.second == op2)
+            EXPECT_EQ(term.first, -0.5i);
+        else
+            ADD_FAILURE() << "Result IS EXPECTED:" << term_2hat_jw_exp << "\nterm_2hat_jw:\n" << term_2hat_jw.to_string();
+    }
 }
 
 TEST(FermionOperatorTest, MultiActionJordanWignerTest1) {
@@ -141,26 +186,73 @@ TEST(FermionOperatorTest, MultiActionJordanWignerTest1) {
 
     Observable op3 = op1_jw + op2_jw;
 
+    // TEST
+
+    MultiQubitPauliOperator ex1("Y 0 Z 1 X 2");
+    MultiQubitPauliOperator ex2("Y 0 Z 1 Y 2");
+    MultiQubitPauliOperator ex3("X 0 Z 1 X 2");
+    MultiQubitPauliOperator ex4("X 0 Z 1 Y 2");
+
     std::string op1_expected =
         "(0-0.25j) [Y 0 Z 1 X 2 ] +\n"
         "(-0.25-0j) [Y 0 Z 1 Y 2 ] +\n"
         "(-0.25+0j) [X 0 Z 1 X 2 ] +\n"
         "(0+0.25j) [X 0 Z 1 Y 2 ]";
+    EXPECT_EQ(op1_jw.get_term_count(), 4);
+    for(int i = 0; i < op1_jw.get_term_count(); i++) {
+        auto term = op1_jw.get_term(i);
+        if(term.second == ex1)
+            EXPECT_EQ(term.first, -0.25i);
+        else if(term.second == ex2)
+            EXPECT_EQ(term.first, -0.25);
+        else if(term.second == ex3)
+            EXPECT_EQ(term.first, -0.25);
+        else if(term.second == ex4)
+            EXPECT_EQ(term.first, 0.25i);
+        else
+            ADD_FAILURE() << "Result IS EXPECTED:\n" << op1_expected << "\nop1_jw:\n" << op1_jw.to_string();
+    }
+
     std::string op2_expected =
         "(0-0.25j) [Y 0 Z 1 X 2 ] +\n"
         "(0.25+0j) [Y 0 Z 1 Y 2 ] +\n"
         "(0.25+0j) [X 0 Z 1 X 2 ] +\n"
         "(0+0.25j) [X 0 Z 1 Y 2 ]";
+    EXPECT_EQ(op2_jw.get_term_count(), 4);
+    for(int i = 0; i < op2_jw.get_term_count(); i++) {
+        auto term = op2_jw.get_term(i);
+        if(term.second == ex1)
+            EXPECT_EQ(term.first, -0.25i);
+        else if(term.second == ex2)
+            EXPECT_EQ(term.first, 0.25);
+        else if(term.second == ex3)
+            EXPECT_EQ(term.first, 0.25);
+        else if(term.second == ex4)
+            EXPECT_EQ(term.first, 0.25i);
+        else
+            ADD_FAILURE() << "Result IS EXPECTED:\n" << op2_expected << "\nop2_jw:\n" << op2_jw.to_string();
+    }
+    
     // TODO 係数が0の場合、Observableから削除する処理をする
     std::string op3_expected =
         "(0-0.5j) [Y 0 Z 1 X 2 ] +\n"
         "(0+0j) [Y 0 Z 1 Y 2 ] +\n"
         "(0+0j) [X 0 Z 1 X 2 ] +\n"
         "(0+0.5j) [X 0 Z 1 Y 2 ]";
-
-    EXPECT_EQ(op1_jw.to_string(), op1_expected);
-    EXPECT_EQ(op2_jw.to_string(), op2_expected);
-    EXPECT_EQ(op3.to_string(), op3_expected);
+    EXPECT_EQ(op3.get_term_count(), 4);
+    for(int i = 0; i < op3.get_term_count(); i++) {
+        auto term = op3.get_term(i);
+        if(term.second == ex1)
+            EXPECT_EQ(term.first, -0.5i);
+        else if(term.second == ex2)
+            EXPECT_EQ(term.first, 0i);
+        else if(term.second == ex3)
+            EXPECT_EQ(term.first, 0i);
+        else if(term.second == ex4)
+            EXPECT_EQ(term.first, 0.5i);
+        else
+            ADD_FAILURE() << "Result IS EXPECTED:\n" << op3_expected << "\nop3:\n" << op3.to_string();
+    }
 }
 
 TEST(FermionOperatorTest, MultiActionJordanWignerTest2) {
@@ -173,16 +265,52 @@ TEST(FermionOperatorTest, MultiActionJordanWignerTest2) {
 
     Observable op3 = op1_jw + op2_jw;
 
+    // TEST
+
+    MultiQubitPauliOperator ex1("Y 0 Z 1 X 2");
+    MultiQubitPauliOperator ex2("Y 0 Z 1 Y 2");
+    MultiQubitPauliOperator ex3("X 0 Z 1 X 2");
+    MultiQubitPauliOperator ex4("X 0 Z 1 Y 2");
+
     std::string op1_expected =
-        "(0-0.25j) [Y 0 Z 1 X 2 ] +\n"
-        "(-0.25-0j) [Y 0 Z 1 Y 2 ] +\n"
-        "(-0.25+0j) [X 0 Z 1 X 2 ] +\n"
-        "(0+0.25j) [X 0 Z 1 Y 2 ]";
-    std::string op2_expected =
-        "(0-0.25j) [Y 0 Z 1 X 2 ] +\n"
+        "(0+0.25j) [Y 0 Z 1 X 2 ] +\n"
         "(0.25+0j) [Y 0 Z 1 Y 2 ] +\n"
         "(0.25+0j) [X 0 Z 1 X 2 ] +\n"
-        "(0+0.25j) [X 0 Z 1 Y 2 ]";
+        "(0-0.25j) [X 0 Z 1 Y 2 ]";
+    EXPECT_EQ(op1_jw.get_term_count(), 4);
+    for(int i = 0; i < op1_jw.get_term_count(); i++) {
+        auto term = op1_jw.get_term(i);
+        if(term.second == ex1)
+            EXPECT_EQ(term.first, 0.25i);
+        else if(term.second == ex2)
+            EXPECT_EQ(term.first, 0.25);
+        else if(term.second == ex3)
+            EXPECT_EQ(term.first, 0.25);
+        else if(term.second == ex4)
+            EXPECT_EQ(term.first, -0.25i);
+        else
+            ADD_FAILURE() << "Result IS EXPECTED:\n" << op1_expected << "\nop1_jw:\n" << op1_jw.to_string();
+    }
+
+    std::string op2_expected =
+        "(0+0.25j) [Y 0 Z 1 X 2 ] +\n"
+        "(-0.25+0j) [Y 0 Z 1 Y 2 ] +\n"
+        "(-0.25+0j) [X 0 Z 1 X 2 ] +\n"
+        "(0-0.25j) [X 0 Z 1 Y 2 ]";
+    EXPECT_EQ(op2_jw.get_term_count(), 4);
+    for(int i = 0; i < op2_jw.get_term_count(); i++) {
+        auto term = op2_jw.get_term(i);
+        if(term.second == ex1)
+            EXPECT_EQ(term.first, -0.25i);
+        else if(term.second == ex2)
+            EXPECT_EQ(term.first, 0.25);
+        else if(term.second == ex3)
+            EXPECT_EQ(term.first, 0.25);
+        else if(term.second == ex4)
+            EXPECT_EQ(term.first, 0.25i);
+        else
+            ADD_FAILURE() << "Result IS EXPECTED:\n" << op2_expected << "\nop2_jw:\n" << op2_jw.to_string();
+    }
     // TODO 係数が0の場合、Observableから削除する処理をする
     std::string op3_expected =
         "(0+0j) [Y 0 Z 1 X 2 ] +\n"
@@ -190,7 +318,20 @@ TEST(FermionOperatorTest, MultiActionJordanWignerTest2) {
         "(0.5+0j) [Y 0 Z 1 Y 2 ] +\n"
         "(0+0j) [X 0 Z 1 Y 2 ]";
 
-    EXPECT_EQ(op3.to_string(), op3_expected);
+    EXPECT_EQ(op3.get_term_count(), 4);
+    for(int i = 0; i < op3.get_term_count(); i++) {
+        auto term = op3.get_term(i);
+        if(term.second == ex1)
+            EXPECT_EQ(term.first, 0i);
+        else if(term.second == ex2)
+            EXPECT_EQ(term.first, 0.5);
+        else if(term.second == ex3)
+            EXPECT_EQ(term.first, 0.5);
+        else if(term.second == ex4)
+            EXPECT_EQ(term.first, 0i);
+        else
+            ADD_FAILURE() << "Result IS EXPECTED:\n" << op3_expected << "\nop3:\n" << op3.to_string();
+    }
 }
 
 TEST(FermionOperatorTest, count_qubitsTest){
