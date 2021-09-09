bc210fe7c360d5b2a8a5a3f7965147fb90e120b9
kotamanegi <kotamanegi84@gmail.com>
2021-09-09 18:17:52 +0900

    [Feat #116] :shower: remove USE_MPI flag

--- script/generate_msvc_project_2015_with_gpu.bat
+++ script/generate_msvc_project_2015_with_gpu.bat
@@ -1,4 +1,4 @@
 mkdir visualstudio
 cd visualstudio
-cmake -G "Visual Studio 14 2015 Win64" -D USE_GPU:STR=Yes -D USE_MPI:STR=No ..
+cmake -G "Visual Studio 14 2015 Win64" -D USE_GPU:STR=Yes ..
 cd ..
