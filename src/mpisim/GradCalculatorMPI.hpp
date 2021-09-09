2b81ab4bc27565834e8898d228c326ad18555286
kotamanegi <kotamanegi84@gmail.com>
2021-09-09 18:13:18 +0900

    [Feat #116] :shower: remove mpisim

--- src/mpisim/GradCalculatorMPI.hpp
+++
@@ -1,9 +0,0 @@
-#include "cppsim/observable.hpp"
-#include "cppsim/state.hpp"
-#include "vqcsim/parametric_circuit.hpp"
-
-class DllExport GradCalculatorMPI {
-public:
-    std::vector<std::complex<double>> calculate_grad(
-        ParametricQuantumCircuit& x, Observable& obs, std::vector<double> theta);
-};
