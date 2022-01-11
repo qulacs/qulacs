2fe416d57fc9aa601931806ae02c4e0e367a8bca
kosukemtr <38150579+kosukemtr@users.noreply.github.com>
2022-01-11 15:42:10 +0900

    formatting

--- test/cppsim/test_noisyevolution.cpp
+++ test/cppsim/test_noisyevolution.cpp
@@ -175,7 +175,8 @@ TEST(NoisyEvolutionTest, dephasing) {
     for (int k = 0; k < n_samples; k++) {
         state.load(&init_state);
         circuit.update_quantum_state(&state);
-        exp += observable.get_expectation_value_single_thread(&state).real() / n_samples;
+        exp += observable.get_expectation_value_single_thread(&state).real() /
+               n_samples;
     }
     // intrinsic variance is (<P>_withoutnoise^2-<P>_withnoise^2).
     // take 5-sigma for assertion. Correct code should violate this assertion by
@@ -223,7 +224,8 @@ TEST(NoisyEvolutionTest, T1T2) {
         gate::H(0)->update_quantum_state(&state);
         gate::H(1)->update_quantum_state(&state);
         circuit.update_quantum_state(&state);
-        exp += observable.get_expectation_value_single_thread(&state).real() / n_samples;
+        exp += observable.get_expectation_value_single_thread(&state).real() /
+               n_samples;
     }
     std::cout << "NoisyEvolution: " << exp << " ref: " << ref << std::endl;
     ASSERT_NEAR(exp, ref, .1);
@@ -256,7 +258,7 @@ TEST(NoisyEvolutionTest, check_inf_occurence) {
     double exp = 0.;
     for (int k = 0; k < n_samples; k++) {
         circuit.update_quantum_state(&state);
-        ASSERT_FALSE(
-            std::isinf(observable.get_expectation_value_single_thread(&state).real()));
+        ASSERT_FALSE(std::isinf(
+            observable.get_expectation_value_single_thread(&state).real()));
     }
 }
