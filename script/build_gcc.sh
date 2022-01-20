9b458b30eed38601783607111b499d7dc5734f44
ikanago <ikanago-dev@protonmail.com>
2022-01-20 06:42:40 +0000

    Specify OUTPUT_DIRECTORYs properly
    Generate test binaries into bin/ and .so into build/lib.linux-x86_64-3.9/ .

--- script/build_gcc.sh
+++ script/build_gcc.sh
@@ -5,12 +5,9 @@ set -eu
 GCC_COMMAND=${C_COMPILER:-"gcc"}
 GXX_COMMAND=${CXX_COMPILER:-"g++"}
 OPT_FLAGS=${QULACS_OPT_FLAGS:-"-mtune=native -march=native -mfpmath=both"}
-# Tell the Python interpreter path to pybind.
-PYTHON_EXECUTABLE=$(python -c "import sys; print(sys.executable)")
 
 mkdir -p ./build
 cd ./build
-cmake -G "Unix Makefiles" -D CMAKE_C_COMPILER=$GCC_COMMAND -D CMAKE_CXX_COMPILER=$GXX_COMMAND -D OPT_FLAGS="$OPT_FLAGS" -D CMAKE_BUILD_TYPE=Release USE_GPU:STR=No -D PYTHON_EXECUTABLE="$PYTHON_EXECUTABLE" ..
+cmake -G "Unix Makefiles" -D CMAKE_C_COMPILER=$GCC_COMMAND -D CMAKE_CXX_COMPILER=$GXX_COMMAND -D OPT_FLAGS="$OPT_FLAGS" -D CMAKE_BUILD_TYPE=Release USE_GPU:STR=No ..
 make -j $(nproc)
-make python -j $(nproc)
 cd ../
