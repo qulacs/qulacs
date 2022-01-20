119afa1927713d43f2a20471d9c3a3b64fc23677
ikanago <ikanago-dev@protonmail.com>
2022-01-20 06:44:36 +0000

    Format code

--- setup.py
+++ setup.py
@@ -8,28 +8,30 @@ from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
 from distutils.version import LooseVersion
 
-_VERSION = '0.2.0'
+VERSION = "0.2.0"
+PROJECT_NAME = "qulacs-osaka"
 
-project_name = 'qulacs-osaka'
 
 def _is_valid_compiler(cmd):
     try:
-        out = subprocess.check_output([cmd, '-dumpfullversion', '-dumpversion']).decode()
+        out = subprocess.check_output(
+            [cmd, "-dumpfullversion", "-dumpversion"]
+        ).decode()
         version = LooseVersion(out)
-        return version >= LooseVersion('7.0.0')
+        return version >= LooseVersion("7.0.0")
     except:
         return False
 
 
 class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=''):
+    def __init__(self, name, sourcedir=""):
         Extension.__init__(self, name, sources=[])
         self.sourcedir = os.path.abspath(sourcedir)
 
 
 class CMakeBuild(build_ext):
     user_options = build_ext.user_options + [
-        ('opt-flags=', 'o', 'optimization flags for compiler')
+        ("opt-flags=", "o", "optimization flags for compiler")
     ]
 
     def initialize_options(self):
@@ -41,12 +43,16 @@ class CMakeBuild(build_ext):
 
     def run(self):
         try:
-            out = subprocess.check_output(['cmake', '--version'])
+            out = subprocess.check_output(["cmake", "--version"])
         except OSError:
-            raise RuntimeError("CMake must be installed to build the following extensions: " +
-                               ", ".join(e.name for e in self.extensions))
+            raise RuntimeError(
+                "CMake must be installed to build the following extensions: "
+                + ", ".join(e.name for e in self.extensions)
+            )
 
-        cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
+        cmake_version = LooseVersion(
+            re.search(r"version\s*([\d.]+)", out.decode()).group(1)
+        )
         for ext in self.extensions:
             self.build_extension(ext)
 
@@ -55,26 +61,31 @@ class CMakeBuild(build_ext):
 
         if self.opt_flags is not None:
             opt_flags = self.opt_flags
-        elif os.getenv('QULACS_OPT_FLAGS'):
-            opt_flags = os.getenv('QULACS_OPT_FLAGS')
+        elif os.getenv("QULACS_OPT_FLAGS"):
+            opt_flags = os.getenv("QULACS_OPT_FLAGS")
         else:
             opt_flags = None
         if opt_flags:
-            cmake_args += ['-DOPT_FLAGS=' + opt_flags]
+            cmake_args += ["-DOPT_FLAGS=" + opt_flags]
 
         cmake_args += ["-DUSE_GPU:STR=No"]
 
         env = os.environ.copy()
-        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
-                                                              self.distribution.get_version())
+        env["CXXFLAGS"] = '{} -DVERSION_INFO=\\"{}\\"'.format(
+            env.get("CXXFLAGS", ""), self.distribution.get_version()
+        )
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
 
         build_dir = os.path.join(os.getcwd(), "build")
         os.makedirs(build_dir, exist_ok=True)
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=build_dir, env=env)
+        subprocess.check_call(
+            ["cmake", ext.sourcedir] + cmake_args, cwd=build_dir, env=env
+        )
         print(build_args, self.build_temp, cmake_args)
-        subprocess.check_call(['cmake', '--build', '.', '--target', 'python'] + build_args, cwd=build_dir)
+        subprocess.check_call(
+            ["cmake", "--build", ".", "--target", "python"] + build_args, cwd=build_dir
+        )
 
     def _generate_args(self, ext):
         # Following directories are created by cmake automatically.
@@ -85,59 +96,67 @@ class CMakeBuild(build_ext):
         # Directory to output test binaries.
         bindir = os.path.join(os.getcwd(), "bin")
         cmake_args = [
-                      '-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY=' + archive_dir,
-                      '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=' + bindir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable,
-                      '-DPYTHON_SETUP_FLAG:STR=Yes']
+            "-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY=" + archive_dir,
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir,
+            "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=" + bindir,
+            "-DPYTHON_EXECUTABLE=" + sys.executable,
+            "-DPYTHON_SETUP_FLAG:STR=Yes",
+        ]
 
-        cfg = 'Debug' if self.debug else 'Release'
+        cfg = "Debug" if self.debug else "Release"
         # TODO: Use -j$(nproc)
-        build_args = ['--config', cfg, '--', '-j']
+        build_args = ["--config", cfg, "--", "-j"]
 
         if platform.system() == "Windows":
-            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
-            cmake_args += ['-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
-            if sys.maxsize > 2**32:
-                cmake_args += ['-A', 'x64']
-            build_args += ['--', '/m']
+            cmake_args += [
+                "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)
+            ]
+            cmake_args += [
+                "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)
+            ]
+            if sys.maxsize > 2 ** 32:
+                cmake_args += ["-A", "x64"]
+            build_args += ["--", "/m"]
         else:
-            gcc = os.getenv('C_COMPILER', 'gcc')
-            gxx = os.getenv('CXX_COMPILER', 'g++')
+            gcc = os.getenv("C_COMPILER", "gcc")
+            gxx = os.getenv("CXX_COMPILER", "g++")
             if gcc is None or gxx is None:
-                raise RuntimeError("gcc/g++ must be installed to build the following extensions: " +
-                               ", ".join(e.name for e in self.extensions))
+                raise RuntimeError(
+                    "gcc/g++ must be installed to build the following extensions: "
+                    + ", ".join(e.name for e in self.extensions)
+                )
 
-            cmake_args += ['-DCMAKE_C_COMPILER=' + gcc]
-            cmake_args += ['-DCMAKE_CXX_COMPILER=' + gxx]
-            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            cmake_args += ["-DCMAKE_C_COMPILER=" + gcc]
+            cmake_args += ["-DCMAKE_CXX_COMPILER=" + gxx]
+            cmake_args += ["-DCMAKE_BUILD_TYPE=" + cfg]
 
         return build_args, cmake_args
 
+
 setup(
-    name=project_name,
-    version=_VERSION,
-    author='QunaSys',
-    author_email='qulacs@qunasys.com',
-    url='http://www.qulacs.org',
-    description='Quantum circuit simulator for research',
-    long_description='',
-    package_dir = {"": "pysrc"},
-    packages=find_packages(exclude=['test*'])+find_packages("pysrc"),
+    name=PROJECT_NAME,
+    version=VERSION,
+    author="QunaSys",
+    author_email="qulacs@qunasys.com",
+    url="http://www.qulacs.org",
+    description="Quantum circuit simulator for research",
+    long_description="",
+    package_dir={"": "pysrc"},
+    packages=find_packages(exclude=["test*"]) + find_packages("pysrc"),
     include_package_data=True,
-    ext_modules=[CMakeExtension('qulacs_core')],
+    ext_modules=[CMakeExtension("qulacs_core")],
     cmdclass=dict(build_ext=CMakeBuild),
     zip_safe=False,
-    test_suite = 'test',
+    test_suite="test",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Programming Language :: Python',
-        'Topic :: Communications :: Email',
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: Communications :: Email",
     ],
 )
