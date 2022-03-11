f7de97eda6a654ec045857660b1d1769e00acde7
KowerKoint <kowerkoint@gmail.com>
2022-03-11 15:29:44 +0900

    Implement specific exception in vqcsim

--- src/vqcsim/parametric_circuit.cpp
+++ src/vqcsim/parametric_circuit.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 
+#include "cppsim/exception.hpp"
 #include "cppsim/gate_factory.hpp"
 #include "cppsim/gate_matrix.hpp"
 #include "cppsim/gate_merge.hpp"
@@ -225,7 +226,7 @@ std::vector<double> ParametricQuantumCircuit::backprop_inner_product(
                 error_message_stream
                     << "Error: " << gate_now->get_name()
                     << " does not support backprop in parametric";
-                throw std::invalid_argument(error_message_stream.str());
+                throw NotImplementedException(error_message_stream.str());
             }
             RcPI->update_quantum_state(Astate);
             ans[inverse_parametric_gate_position[i]] =
