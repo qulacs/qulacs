5d47a7d487c1a7181ae6542691d94e1fcad3d333
kotamanegi <kotamanegi84@gmail.com>
2022-05-17 08:56:18 +0000

    fix broken __init__.py

--- pysrc/qulacs/__init__.py
+++ pysrc/qulacs/__init__.py
@@ -1,7 +1,6 @@
 from qulacs_core import *
 
 import qulacs.observable._get_matrix
-import qulacs.pickle_QuantumState
 
 Observable.get_matrix = \
     lambda obs: qulacs.observable._get_matrix._get_matrix(obs)
