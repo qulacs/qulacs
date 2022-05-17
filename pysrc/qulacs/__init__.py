911de7bbf9dd13f921e375934e9924e5aba267c0
kotamanegi <kotamanegi84@gmail.com>
2022-05-17 08:48:25 +0000

    Remove qulacs-osaka

--- pysrc/qulacs/__init__.py
+++ pysrc/qulacs/__init__.py
@@ -1,5 +1,7 @@
 from qulacs_core import *
+
 import qulacs.observable._get_matrix
+import qulacs.pickle_QuantumState
 
 Observable.get_matrix = \
     lambda obs: qulacs.observable._get_matrix._get_matrix(obs)
--- src/cppsim_experimental/gate.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "gate_base.hpp"
-#include "gate_basic.hpp"
-#include "gate_wrapper.hpp"
