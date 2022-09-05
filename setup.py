cdd65db176197033855344fc6a81d4e9e42bb5ca
Toshio Mori <forest1040@gmail.com>
2022-09-05 16:15:13 +0900

    Revert "v0.5.0"
    This reverts commit 5c0c29fd9c9642ce51dcc7fa76c412df42d15d3d.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 from setuptools import Extension, find_packages, setup
 from setuptools.command.build_ext import build_ext
 
-VERSION = "0.5.0"
+VERSION = "0.4.2"
 PROJECT_NAME = "qulacs"
 
 
