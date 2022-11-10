37ed817fa2f5242fea91a45fd6df61d0129b6040
kotamanegi <kotamanegi84@gmail.com>
2022-11-10 07:07:45 +0000

    Inject memory leak for CI check

--- test/cppsim/test_gate_dm.cpp
+++ test/cppsim/test_gate_dm.cpp
@@ -128,8 +128,6 @@ TEST(DensityMatrixGateTest, ApplySingleQubitRotationGate) {
                     ASSERT_NEAR(abs(state.data_cpp()[i * dim + j] -
                                     dm_test.data_cpp()[i * dim + j]),
                         0, eps);
-
-            delete gate;
         }
     }
 }
