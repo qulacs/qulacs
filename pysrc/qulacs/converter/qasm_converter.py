afd3ec4e58683bc10dee52590a2be92e033d8e92
kotamanegi <kotamanegi84@gmail.com>
2022-11-25 02:09:14 +0000

    replace parse library with standard library

--- pysrc/qulacs/converter/qasm_converter.py
+++ pysrc/qulacs/converter/qasm_converter.py
@@ -1,3 +1,4 @@
+import re
 import typing
 from cmath import phase
 from logging import NullHandler, getLogger
@@ -5,7 +6,6 @@ from typing import List
 
 import numpy as np
 import qulacs_core
-from parse import parse, search
 from qulacs import QuantumCircuit
 from qulacs.gate import DenseMatrix, Identity
 
@@ -117,118 +117,178 @@ def convert_QASM_to_qulacs_circuit(
     for instr_moto in input_strs:
         # process input string for parsing instruction.
         instr = instr_moto.lower().strip().replace(" ", "").replace("\t", "")
-
+        
         if instr[0:4] == "qreg":
-            ary = parse("qregq[{:d}];", instr)
-            cir = QuantumCircuit(ary[0])
+            matchobj = re.match(r'qregq\[(\d+)\];', instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
+            cir = QuantumCircuit(int(ary[0]))
             if len(mapping) == 0:
-                mapping = list(range(ary[0]))
+                mapping = list(range(int(ary[0])))
         elif instr[0:2] == "cx":
-            ary = parse("cxq[{:d}],q[{:d}];", instr)
-            cir.add_CNOT_gate(mapping[ary[0]], mapping[ary[1]])
+            matchobj = re.match(r"cxq\[(\d+)\],q\[(\d+)\];", instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
+            cir.add_CNOT_gate(mapping[int(ary[0])], mapping[int(ary[1])])
         elif instr[0:2] == "cz":
-            ary = parse("czq[{:d}],q[{:d}];", instr)
-            cir.add_CZ_gate(mapping[ary[0]], mapping[ary[1]])
+            matchobj = re.match(r"czq\[(\d+)\],q\[(\d+)\];", instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
+            cir.add_CZ_gate(mapping[int(ary[0])], mapping[int(ary[1])])
         elif instr[0:4] == "swap":
-            ary = parse("swapq[{:d}],q[{:d}];", instr)
-            cir.add_SWAP_gate(mapping[ary[0]], mapping[ary[1]])
+            matchobj = re.match(r"swapq\[(\d+)\],q\[(\d+)\];", instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
+            cir.add_SWAP_gate(mapping[int(ary[0])], mapping[int(ary[1])])
         elif instr[0:2] == "id":
-            ary = parse("idq[{:d}];", instr)
-            cir.add_gate(Identity(mapping[ary[0]]))
+            matchobj = re.match(r"idq\[(\d+)\];", instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
+            cir.add_gate(Identity(mapping[int(ary[0])]))
         elif instr[0:2] == "xq":
-            ary = parse("xq[{:d}];", instr)
-            cir.add_X_gate(mapping[ary[0]])
+            matchobj = re.match(r"xq\[(\d+)\];", instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
+            cir.add_X_gate(mapping[int(ary[0])])
         elif instr[0:2] == "yq":
-            ary = parse("yq[{:d}];", instr)
-            cir.add_Y_gate(mapping[ary[0]])
+            matchobj = re.match(r"yq\[(\d+)\];", instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
+            cir.add_Y_gate(mapping[int(ary[0])])
         elif instr[0:2] == "zq":
-            ary = parse("zq[{:d}];", instr)
-            cir.add_Z_gate(mapping[ary[0]])
+            matchobj = re.match(r"zq\[(\d+)\];", instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
+            cir.add_Z_gate(mapping[int(ary[0])])
         elif instr[0:2] == "hq":
-            ary = parse("hq[{:d}];", instr)
-            cir.add_H_gate(mapping[ary[0]])
+            matchobj = re.match(r"hq\[(\d+)\];", instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
+            cir.add_H_gate(mapping[int(ary[0])])
         elif instr[0:2] == "sq":
-            ary = parse("sq[{:d}];", instr)
-            cir.add_S_gate(mapping[ary[0]])
+            matchobj = re.match(r"sq\[(\d+)\];", instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
+            cir.add_S_gate(mapping[int(ary[0])])
         elif instr[0:4] == "sdgq":
-            ary = parse("sdgq[{:d}];", instr)
-            cir.add_Sdag_gate(mapping[ary[0]])
+            matchobj = re.match(r"sdgq\[(\d+)\];", instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
+            cir.add_Sdag_gate(mapping[int(ary[0])])
         elif instr[0:2] == "tq":
-            ary = parse("tq[{:d}];", instr)
-            cir.add_T_gate(mapping[ary[0]])
+            matchobj = re.match(r"tq\[(\d+)\];", instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
+            cir.add_T_gate(mapping[int(ary[0])])
         elif instr[0:4] == "tdgq":
-            ary = parse("tdgq[{:d}];", instr)
-            cir.add_Tdag_gate(mapping[ary[0]])
+            matchobj = re.match(r"tdgq\[(\d+)\];", instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
+            cir.add_Tdag_gate(mapping[int(ary[0])])
         elif instr[0:2] == "rx":
-            ary = parse("rx({:g})q[{:d}];", instr)
-            cir.add_RX_gate(mapping[ary[1]], -ary[0])
+            matchobj = re.match(
+                r"rx\((\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?\)q\[(\d+)\];", instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
+            cir.add_RX_gate(mapping[int(ary[1])], -float(ary[0]))
         elif instr[0:2] == "ry":
-            ary = parse("ry({:g})q[{:d}];", instr)
-            cir.add_RY_gate(mapping[ary[1]], -ary[0])
+            matchobj = re.match(
+                r"ry\((\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?\)q\[(\d+)\];", instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
+            cir.add_RY_gate(mapping[int(ary[1])], -float(ary[0]))
         elif instr[0:2] == "rz":
-            ary = parse("rz({:g})q[{:d}];", instr)
-            cir.add_RZ_gate(mapping[ary[1]], -ary[0])
+            matchobj = re.match(
+                r"rz\((\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?\)q\[(\d+)\];", instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
+            cir.add_RZ_gate(mapping[int(ary[1])], -float(ary[0]))
         elif instr[0:1] == "p":
-            ary = parse("p({:g})q[{:d}];", instr)
-            cir.add_U1_gate(mapping[ary[1]], ary[0])
+            matchobj = re.match(
+                r"p\((\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?\)q\[(\d+)\];", instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
+            cir.add_U1_gate(mapping[int(ary[1])], float(ary[0]))
         elif instr[0:2] == "u1":
-            ary = parse("u1({:g})q[{:d}];", instr)
-            cir.add_U1_gate(mapping[ary[1]], ary[0])
+            matchobj = re.match(
+                r"u1\((\d+(\.\d*)?|\.\d+)([eE][-+]?\d+\)?)q[(\d+)];", instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
+            cir.add_U1_gate(mapping[int(ary[1])], float(ary[0]))
         elif instr[0:2] == "u2":
-            ary = parse("u2({:g},{:g})q[{:d}];", instr)
-            cir.add_U2_gate(mapping[ary[2]], ary[0], ary[1])
+            matchobj = re.match(
+                r"u2\((\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?,"
+                + r"(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?\)q\[(\d+)\];", instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
+            cir.add_U2_gate(mapping[int(ary[2])], float(ary[0]), float(ary[1]))
         elif instr[0:2] == "u3":
-            ary = parse("u3({:g},{:g},{:g})q[{:d}];", instr)
-            cir.add_U3_gate(mapping[ary[3]], ary[0], ary[1], ary[2])
+            matchobj = re.match(
+                r"u3\((\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?,"
+                + r"(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?,"
+                + r"(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?\)q\[(\d+)\];", instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
+            cir.add_U3_gate(mapping[int(ary[3])], 
+                            float(ary[0]), float(ary[1]), float(ary[2]))
         elif instr[0:1] == "u":
-            ary = parse("u({:g},{:g},{:g})q[{:d}];", instr)
-            cir.add_U3_gate(mapping[ary[3]], ary[0], ary[1], ary[2])
+            matchobj = re.match(
+                r"u\((\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?,"
+                + r"(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?,"
+                + r"(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?\)q\[(\d+)\];", instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
+            cir.add_U3_gate(mapping[int(ary[3])], 
+                            float(ary[0]), float(ary[1]), float(ary[2]))
         elif instr[0:3] == "sxq":
-            ary = parse("sxq[{:d}];", instr)
-            cir.add_sqrtX_gate(mapping[ary[0]])
+            matchobj = re.match(r"sxq\[(\d+)\];", instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
+            cir.add_sqrtX_gate(mapping[int(ary[0])])
         elif instr[0:5] == "sxdgq":
-            ary = parse("sxdgq[{:d}];", instr)
-            cir.add_sqrtXdag_gate(mapping[ary[0]])
+            matchobj = re.match(r"sxdgq\[(\d+)\];", instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
+            cir.add_sqrtXdag_gate(mapping[int(ary[0])])
         elif instr[0:11] == "densematrix":
-            ary = search("densematrix({:d},{:d}", instr)
-            parsestr = "densematrix({:d},{:d}"
-            for i in range(4 ** ary[0]):
-                parsestr += ",{:g},{:g}"
-            for i in range(ary[1]):
-                parsestr += ",{:d}"
-            parsestr += ")q[{:d}]"
-            for i in range(ary[0] + ary[1] - 1):
-                parsestr += ",q[{:d}]"
-            parsestr += ";"
-            deary = parse(parsestr, instr)
-            gate_mat = np.zeros((2 ** ary[0], 2 ** ary[0]), dtype="complex")
+            deary = re.findall(r"[-+]?\d*\.?\d+|[-+]?\d+", instr)
+            target_qubit_count = int(deary[0])
+            control_qubit_count = int(deary[1])
+            
+            gate_mat = np.zeros((2 ** target_qubit_count,
+                                 2 ** target_qubit_count),
+                                dtype="complex")
             bas = 2
-            for i in range(2 ** ary[0]):
-                for j in range(2 ** ary[0]):
-                    gate_mat[i][j] = deary[bas] + deary[bas + 1] * 1.0j
+            for i in range(2 ** target_qubit_count):
+                for j in range(2 ** target_qubit_count):
+                    gate_mat[i][j] = float(deary[bas]) + float(deary[bas + 1]) * 1.0j
                     bas += 2
             control_values = []
-            for i in range(ary[1]):
-                control_values.append(mapping[deary[bas]])
+            for i in range(control_qubit_count):
+                control_values.append(mapping[int(deary[bas])])
                 bas += 1
             terget_indexes = []
-            for i in range(ary[0]):
-                terget_indexes.append(mapping[deary[bas]])
+            for i in range(target_qubit_count):
+                terget_indexes.append(mapping[int(deary[bas])])
                 bas += 1
 
             dense_gate = DenseMatrix(terget_indexes, gate_mat)  # type:ignore
-            for i in range(ary[1]):
-                control_index = deary[bas]
+            for i in range(control_qubit_count):
+                control_index = int(deary[bas])
                 bas += 1
                 dense_gate.add_control_qubit(control_index, control_values[i])
             cir.add_gate(dense_gate)
         elif remap_remove and instr[0:4] == "//q[":
-            ary = parse("//q[{:d}]-->q[{:d}]", instr)
+            matchobj = re.match(r"//q\[(\d+)-->q\[(\d+)\]", instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
             if not (ary is None):
-                mapping[ary[0]] = ary[1]
+                mapping[int(ary[0])] = int(ary[1])
         elif remap_remove and instr[0:8] == "//qubits":
-            ary = parse("//qubits:{:d}", instr)
-            mapping = list(range(ary[0]))
+            matchobj = re.match(r"//qubits:(\d+)", instr)
+            assert matchobj is not None
+            ary = matchobj.groups()
+            mapping = list(range(int(ary[0])))
         elif instr == "openqasm2.0;" or instr == 'include"qelib1.inc";':
             # related to file format, not for read.
             pass
