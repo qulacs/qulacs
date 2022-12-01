5f3962dd21933a17db7e4f91e2ee17550e999e1b
kotamanegi <kotamanegi84@gmail.com>
2022-12-01 08:13:55 +0000

    delete state converter

--- pysrc/qulacs/converter/__init__.py
+++ pysrc/qulacs/converter/__init__.py
@@ -1,7 +1,4 @@
 from qulacs.converter.qasm_converter import (convert_QASM_to_qulacs_circuit,
                                              convert_qulacs_circuit_to_QASM)
-from qulacs.converter.state_converter import (convert_qulacs_state_to_strs,
-                                              convert_strs_to_qulacs_state)
 
-__all__ = ['convert_QASM_to_qulacs_circuit', 'convert_qulacs_circuit_to_QASM',
-           'convert_qulacs_state_to_strs', 'convert_strs_to_qulacs_state']
+__all__ = ['convert_QASM_to_qulacs_circuit', 'convert_qulacs_circuit_to_QASM']
