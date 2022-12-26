204a7127c6f5e01d64318d73fdc12ee5ee4c3de3
Takumi-Honda <honda.takumi@fujitsu.com>
2022-12-26 18:04:25 +0900

    [csim] Removed utility_sve.hpp

--- src/csim/utility_sve.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-inline static ITYPE getVecLength(void) { return svcntd(); }
-inline static SV_PRED Svptrue(void) { return svptrue_b64(); }
-inline static SV_FTYPE SvdupF(double val) { return svdup_f64(val); }
-inline static SV_ITYPE SvdupI(UINT val) { return svdup_u64(val); }
-inline static SV_ITYPE SvindexI(UINT base, UINT step) {
-    return svindex_u64(base, step);
-}
