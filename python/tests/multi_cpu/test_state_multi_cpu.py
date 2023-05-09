ddc3e9945cb044ef4b95d9ef8a8c95efc3865eec
Hiroya-W <hyuyukun@gmail.com>
2023-05-09 09:05:51 +0000

    Rewrite multi cpu test using pytest

--- python/tests/multi_cpu/test_state_multi_cpu.py
+++ python/tests/multi_cpu/test_state_multi_cpu.py
@@ -1,45 +1,52 @@
-import unittest
-
 import numpy as np
+import pytest
+from pyparsing import Generator
 
 import qulacs
 
+pytestmark = pytest.mark.skipif(
+    not qulacs.check_build_for_mpi(),
+    reason="To use multi-cpu, qulacs built for mpi is required.",
+)
 
-class TestQuantumState(unittest.TestCase):
-    def setUp(self):
+
+class TestQuantumState:
+    @pytest.fixture
+    def init_state(self, init_mpi) -> Generator[None, None, None]:
+        # from fixture in conftest.py
+        multicpu, mpicomm = init_mpi
+
         self.n = 6
         self.state = qulacs.QuantumState(self.n)
         self.state_multi = qulacs.QuantumState(self.n, True)
         self.dim_all = 2**self.n
         self.dim = 2**self.n
         if multicpu:
-            self.mpirank = mpicomm.Get_rank()
-            self.mpisize = mpicomm.Get_size()
+            self.mpirank = mpicomm.Get_rank()  # type: ignore
+            self.mpisize = mpicomm.Get_size()  # type: ignore
         else:
             self.mpirank = 0
             self.mpisize = 1
         if self.state_multi.get_device_name() == "multi-cpu":
             self.dim //= self.mpisize
 
-    def tearDown(self):
+        yield
         del self.state
         del self.state_multi
 
-    def test_state_dim(self):
+    def test_state_dim(self) -> None:
         vector = self.state_multi.get_vector()
-        self.assertEqual(len(vector), self.dim, msg="check vector size")
+        assert len(vector) == self.dim, "check vector size"
 
-    def test_zero_state(self):
+    def test_zero_state(self) -> None:
         self.state_multi.set_zero_state()
         vector = self.state_multi.get_vector()
         vector_ans = np.zeros(self.dim)
         if self.state_multi.get_device_name() == "cpu" or self.mpirank == 0:
             vector_ans[0] = 1.0
-        self.assertTrue(
-            ((vector - vector_ans) < 1e-10).all(), msg="check set_zero_state"
-        )
+        assert ((vector - vector_ans) < 1e-10).all(), "check set_zero_state"
 
-    def test_comp_basis(self):
+    def test_comp_basis(self) -> None:
         pos = 0b010100
         self.state_multi.set_computational_basis(pos)
         vector = self.state_multi.get_vector()
@@ -49,6 +56,5 @@ class TestQuantumState(unittest.TestCase):
             or self.mpirank == pos // self.dim
         ):
             vector_ans[pos % self.dim] = 1.0
-        self.assertTrue(
-            ((vector - vector_ans) < 1e-10).all(), msg="check set_computational_basis"
-        )
+        assert ((vector - vector_ans) < 1e-10).all(), "check set_computational_basis"
+
