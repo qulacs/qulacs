a662bf97ead51956cbc21251d314a7549a1192fc
Hiroya-W <hyuyukun@gmail.com>
2023-05-09 09:05:31 +0000

    Access MPI variables by pytest.fixture

--- python/tests/multi_cpu/conftest.py
+++ python/tests/multi_cpu/conftest.py
@@ -1,16 +1,26 @@
+from typing import Any, Tuple
+
+import pytest
+
 import qulacs
 
-multicpu = False
-if qulacs.check_build_for_mpi():
-    try:
-        from mpi4py import MPI
 
-        mpicomm = MPI.COMM_WORLD
-        if mpicomm.Get_rank() == 0:
-            print("Test with MPI. size=", mpicomm.Get_size())
+@pytest.fixture(scope="session")
+def init_mpi() -> Tuple[bool, Any]:
+    multicpu = False
+    mpicomm = None
 
-        multicpu = True
-    except Exception as e:
-        print(e)
-        print("To use multi-cpu, the mpi4py library is required.")
-        exit()
+    if qulacs.check_build_for_mpi():
+        try:
+            from mpi4py import MPI
+
+            mpicomm = MPI.COMM_WORLD
+            if mpicomm.Get_rank() == 0:
+                print("Test with MPI. size=", mpicomm.Get_size())
+
+            multicpu = True
+        except Exception as e:
+            print(e)
+            print("To use multi-cpu, the mpi4py library is required.")
+
+    return multicpu, mpicomm
