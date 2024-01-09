54ede9f578ecffb9a3123cd07d51ad4f2f231ead
ikanago <ikanago-dev@protonmail.com>
2024-01-09 16:43:07 +0900

    Enable OpenMP on clang by default

--- script/build_clang.sh
+++ script/build_clang.sh
@@ -7,6 +7,6 @@ GXX_COMMAND=clang++
 
 mkdir -p ./build
 cd ./build
-cmake -G "Unix Makefiles" -D CMAKE_C_COMPILER=$GCC_COMMAND -D CMAKE_CXX_COMPILER=$GXX_COMMAND -D CMAKE_BUILD_TYPE=Release -D USE_GPU:STR=No -D USE_TEST="${USE_TEST:-No}" -D USE_OMP:STR=No ..
+cmake -G "Unix Makefiles" -D CMAKE_C_COMPILER=$GCC_COMMAND -D CMAKE_CXX_COMPILER=$GXX_COMMAND -D CMAKE_BUILD_TYPE=Release -D USE_GPU:STR=No -D USE_TEST="${USE_TEST:-No}" ..
 make -j $(nproc)
 cd ../
