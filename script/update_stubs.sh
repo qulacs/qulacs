3c4fef6df252055c004cae2436adc5fa90101e51
Toshio Mori <forest1040@gmail.com>
2024-02-09 10:23:37 +0900

    wip format

--- script/update_stubs.sh
+++ script/update_stubs.sh
@@ -9,6 +9,6 @@ pybind11-stubgen qulacs --root-suffix '' --numpy-array-remove-parameters -o './t
 cp -R typings/qulacs_core/* pysrc/qulacs/
 find pysrc/ -name __init__.pyi | sed -e 's/__init__.pyi/py.typed/' | xargs touch
 
-# format
-black pysrc
-isort pysrc
+# FIXME: format
+# black pysrc
+# isort pysrc
