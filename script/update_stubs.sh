a1269cf2ac3a9b0c83e43c3c6173bde971eaf594
ikanago <ikanago-dev@protonmail.com>
2024-03-12 06:21:50 +0000

    Remove unnecessary stubgen lines in update_stubs.sh

--- script/update_stubs.sh
+++ script/update_stubs.sh
@@ -4,8 +4,6 @@ set -e
 
 stubgen -p qulacs_core -o typings
 pybind11-stubgen qulacs_core --root-suffix '' --numpy-array-remove-parameters -o './typings'
-stubgen -p qulacs -o typings
-pybind11-stubgen qulacs --root-suffix '' --numpy-array-remove-parameters -o './typings'
 cp -R typings/qulacs_core/* pysrc/qulacs/
 find pysrc/ -name __init__.pyi | sed -e 's/__init__.pyi/py.typed/' | xargs touch
 
