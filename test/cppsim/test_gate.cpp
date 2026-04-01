a09c6ceb3598e833c8fb85cefca7a28a1bef2057
usendon <uxiasendon@gmail.com>
2026-04-01 16:01:47 +0200

    ECR added in ApplyTwoQubitGate TEST (test/cppsim/test_gate.cpp file)

--- test/cppsim/test_gate.cpp
+++ test/cppsim/test_gate.cpp
@@ -255,6 +255,8 @@ TEST(GateTest, ApplyTwoQubitGate) {
     funclist.clear();
     funclist.push_back(
         std::make_pair(gate::SWAP, get_eigen_matrix_full_qubit_SWAP));
+    funclist.push_back(
+        std::make_pair(gate::ECR, get_eigen_matrix_full_qubit_ECR));
     for (UINT repeat = 0; repeat < 10; ++repeat) {
         for (auto func_mat : funclist) {
             UINT control = random.int32() % n;
